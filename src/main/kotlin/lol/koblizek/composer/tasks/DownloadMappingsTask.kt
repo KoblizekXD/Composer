6cc0bd5ef4c3427b9acb02b14f4538ba2a108879
KoblizekXD <janprokupek@centrum.cz>
2023-10-12 15:43:07 +0200

    feat: add DownloadMappingsTask

---
+++ src/main/kotlin/lol/koblizek/composer/tasks/DownloadMappingsTask.kt
@@ -0,0 +1,11 @@
+package lol.koblizek.composer.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+abstract class DownloadMappingsTask : DefaultTask() {
+    @TaskAction
+    fun download() {
+
+    }
+}
