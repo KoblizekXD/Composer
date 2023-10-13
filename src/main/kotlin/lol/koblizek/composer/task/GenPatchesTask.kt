720423854c986c6353f92edf66d60990349a9f1b
KoblizekXD <janprokupek@centrum.cz>
2023-10-13 18:27:58 +0200

    add gradle dependency

---
+++ src/main/kotlin/lol/koblizek/composer/task/GenPatchesTask.kt
@@ -0,0 +1,11 @@
+package lol.koblizek.composer.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+abstract class GenPatchesTask : DefaultTask() {
+    @TaskAction
+    fun generate() {
+
+    }
+}
